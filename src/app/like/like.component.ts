dce16c905bb3ca48e9d24c70327da58cb7842511
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-03-21 12:50:59 +0530

    View Encapsulation and shadowDOM

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-like',
   templateUrl: './like.component.html',
-  styleUrls: ['./like.component.css']
+  styleUrls: ['./like.component.css'],
+  encapsulation: ViewEncapsulation.None
+  // ViewEncapsulation: Enum with values (Emulated, Native or None)
+  // Emulated = angular style of applying shadowDOM or scoped styles
+  // Native = uses browser's Native support of shadowDOM
+  // None = no shadowDOM or scoped styles - styles will leak outside the component
 })
 export class LikeComponent implements OnInit {
   // Input Properties and aliasing
