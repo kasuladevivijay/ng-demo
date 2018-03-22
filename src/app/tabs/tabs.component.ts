306bb0443833d3e941a8ff9a39c5f28d0e093679
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-03-22 10:41:14 +0530

    ngFor and change detection via DOM events

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -12,4 +12,15 @@ export class TabsComponent {
     {id: 2, name: 'Clark'},
     {id: 3, name: 'Diana'},
   ];
+
+  // ngFor looks for change detection when the below functions or events are called
+  // add student
+  add() {
+    this.students.push({id: 4, name: 'Flash'});
+  }
+  // remove a student
+  remove(student) {
+    const index = this.students.indexOf(student);
+    this.students.splice(index, 1);
+  }
 }
