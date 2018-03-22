a1280322caff68847873d39489bdeb410cfeb02d
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-03-22 14:21:33 +0530

    simple bootstrap form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LikeComponent } from './like/like.component';
 import { PanelComponent } from './panel/panel.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { InputFormatDirective } from './input-format.directive';
     LikeComponent,
     PanelComponent,
     TabsComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
