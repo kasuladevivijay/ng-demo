082cb0fd34c0e378654055ef81122e3d42784061
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-03-22 10:58:49 +0530

    ngFor and trackBy for better performance while rendering large lists

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -12,6 +12,7 @@ export class TabsComponent {
     {id: 2, name: 'Clark'},
     {id: 3, name: 'Diana'},
   ];
+  courses;
 
   // ngFor looks for change detection when the below functions or events are called
   // add student
@@ -22,5 +23,17 @@ export class TabsComponent {
   remove(student) {
     const index = this.students.indexOf(student);
     this.students.splice(index, 1);
+  }
+  // Load courses
+  loadCourses() {
+    this.courses = [
+      {id: 1, name: 'Course1'},
+      {id: 2, name: 'Course2'},
+      {id: 3, name: 'Course3'}
+    ];
+  }
+  // trackCourses implementation, takes two args - index and object
+  trackCourses(index, course) {
+    return course ? course.id : undefined;
   }
 }
